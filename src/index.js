3c538eda29c84888cf2d4e05f25102c1274ac353
joyscript <joyscript7@gmail.com>
2022-12-29 18:23:14 +0300

    feat: solve human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
+  const numbers = {
+    tillTwenty: ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+    tens: ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  }
+
+  if (number == 0) return 'zero';
+
+  const getNumTillHundred = (number) => {
+    if(number % 100 == 0) return '';
+    let numStr = number < 100 ? number.toString() : number.toString().slice(1);
+    return numStr < 20 ? numbers.tillTwenty[+numStr]
+      : `${numbers.tens[numStr[0]]} ${numbers.tillTwenty[numStr[1]]}`.trim();
+  }
+
+  return number < 100 ? getNumTillHundred(number)
+    :`${numbers.tillTwenty[number.toString()[0]]} hundred ${getNumTillHundred(number)}`.trim();
 }
